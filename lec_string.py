44d972ce215d30104bfb3a85fc932833c050bb86
Denchik444 <shtunderd07@mail.ru>
2024-09-25 16:04:02 +0000

    делал делал грязь

---
+++ lec_string.py
@@ -0,0 +1,14 @@
+a = 'Good'
+b = 'Bad'
+
+print(a + b)
+
+# print(a - b) NO!
+
+print(a * 3)
+
+c = 50
+d = 10
+print(f'вставь {c} и {d}')
+
+print(len(a))
---
+++ lec_type.base.py
@@ -0,0 +1,23 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('молодчик'))
+
+a = 'молодчик'
+print(type(a))
+
+a = ['хорош', 1, 'неплох', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
+
+print(print())
