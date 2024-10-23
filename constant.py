25167a1f98eae4a7dcf1463be5287b8e35373915
Denchik444 <shtunderd07@mail.ru>
2024-10-23 15:00:24 +0000

    gty

--- constant.py
+++ constant.py
@@ -1,1 +1,11 @@
-https://astromodel.ru/category/obrazovatelnyj-blok/matematicheskoe-modelirovanie-praktika/fizicheskoe-modelirovanie-anonsy-kursov-matematicheskoe-modelirovanie-praktika/praktikum-%E2%84%963-moduli-i-biblioteki/dopolnitelnyj-blok-praktikum-%E2%84%962-czikly-i-vetvleniya-praktikum-%E2%84%963-moduli-i-biblioteki/
+c = 3 * 10**8 # м3 / с — скорость света
+G = 6.67 * (10**(-11)) # м3 / с2 кг — гравитационная постоянная
+R = 8.31 # дж/моль*К универсальная молярная газавая постоянность
+Patmos = 101325 # Па нормальная атмосфера
+F = 9.6 * (10**(4)) # Кл / моль постоянная Фарадея
+aem = 1.66 * (10**(-27)) # кг Атомная единица массы
+Mz = 5.97 * 1024 # кг Масса Земли
+Rz = 6.37 * 106 # м Радиус Земли
+epsilon_0 = 8.85 * (10**(-12)) # ф/м Электрическая постоянная
+μ0 = 12.57 * (10**(-7)) # гн/м Магнитная постоянная
+g = 9.8 # джииии!!!! 
--- task_2.py
+++ task_2.py
@@ -1,8 +1,9 @@
-import 
+from constant import g
+import math
 h = 100
 α = 45
 β = 35
-g = 9.8
 
-SPEED = ((g*h * (math.tan(β) ** 2)) / (2 * (math.tan(α) ** 2) * (1 - math.tan(β) * math.tan(α))) ** 0.5) 
+
+SPEED = ((g * h * (math.tan(β) ** 2)) / (2 * (math.tan(α) ** 2) * (1 - math.tan(β) * math.tan(α))) ** 0.5) 
 print(SPEED)
