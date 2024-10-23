7286434d907bea7bf426537513cec9657d6eeb54
Denchik444 <shtunderd07@mail.ru>
2024-10-23 15:22:48 +0000

    ghjie

--- task_3.py
+++ task_3.py
@@ -2,9 +2,15 @@ from constant import g
 
 time0 = 0
 time = 5
-SPEED = int(input('введите начальную скорость '))
-X0 = int(input('введите начальное расположение по Х '))
-Y0 = int(input('введите начальное расположение по Y '))
+SPEED = input('введите начальную скорость ')
+X0 = input('введите начальное расположение по Х ')
+Y0 = input('введите начальное расположение по Y ')
+X = range(0, 5, 1)
+Y = range(0, 5, 1)
 
-while time0 < time:
-    
+while time0 <= time:
+    X[time0] = X0 + SPEED * time0
+    print(X[time0])
+    Y[time0] = Y0 + SPEED * time0 - ((g * (time0 ** 2)) / 2)
+    print(Y[time0])
+    time0 += 1
