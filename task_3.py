887a85642af61da370ea95e341904681eba3f660
Denchik444 <shtunderd07@mail.ru>
2024-10-23 15:03:01 +0000

    gjcnjq

--- task_3.py
+++ task_3.py
@@ -1,8 +1,10 @@
 from constant import g
 
-time1 = 0
+time0 = 0
 time = 5
 SPEED = int(input('введите начальную скорость '))
 X0 = int(input('введите начальное расположение по Х '))
 Y0 = int(input('введите начальное расположение по Y '))
 
+while time0 < time:
+    
