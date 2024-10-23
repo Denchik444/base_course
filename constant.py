3b54c43e84160c7776f02501d32bc6b54d1e02cd
Denchik444 <shtunderd07@mail.ru>
2024-10-23 14:45:59 +0000

    ytn

---
+++ constant.py
@@ -0,0 +1,1 @@
+https://astromodel.ru/category/obrazovatelnyj-blok/matematicheskoe-modelirovanie-praktika/fizicheskoe-modelirovanie-anonsy-kursov-matematicheskoe-modelirovanie-praktika/praktikum-%E2%84%963-moduli-i-biblioteki/dopolnitelnyj-blok-praktikum-%E2%84%962-czikly-i-vetvleniya-praktikum-%E2%84%963-moduli-i-biblioteki/
