94e2b0f1a412eaa143412fbdef02635b59d18984
Denchik444 <shtunderd07@mail.ru>
2024-10-23 14:39:18 +0000

    kfls

--- task_2.py
+++ task_2.py
@@ -1,7 +1,8 @@
-import numpy as np
+import math
 h = 100
 α = 45
 β = 35
+g = 9.8
 
-
-SPEED = (g*h * (tan))
+SPEED = ((g*h * (math.tan(β) ** 2)) / (2 * (math.tan(α) ** 2) * (1 - math.tan(β) * math.tan(α))) ** 0.5) 
+print(SPEED)
