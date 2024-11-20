68fdeb1c92b95d5d352e43eeb6b8ad2ce2b51873
Denchik444 <shtunderd07@mail.ru>
2024-11-20 14:06:18 +0000

    ghj

--- task_1.py
+++ task_1.py
@@ -15,24 +15,3 @@ print(MASS3)
 
 for i in range(len(N)):
     
-        
-
-
-
-def ariphemetic(array):
-    s = 0
-    for i in range(len(array)):
-        s += array[i]
-    return s / len(array)
-
-a = [0, 1, 2, 3, 4, 5]
-print(ariphemetic(a))
-ariphemetic(a)
-
-def ariphemetic(*a):
-    s = 0
-    for i in range(len(a)):
-        s += a[i]
-    return s / len(a)
-
-print(ariphemetic(0, 1, 2, 3, 4, 5))
