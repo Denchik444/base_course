2ba1fac4fe47837cb44c7ae79f04733eeb76a00d
Denchik444 <shtunderd07@mail.ru>
2024-11-20 13:26:56 +0000

    ytgjyzk

--- task_1.py
+++ task_1.py
@@ -1,7 +1,21 @@
 import numpy as np
 
 
+N = int(input('введите количество '))
 
+MASS1 = np.arange(0, 100, N)
+print(MASS1)
+
+MASS2 = np.arange(0, 100, N)
+print(MASS2)
+
+MASS3 = np.arange(0, 100, N)
+print(MASS3)
+
+W = 
+for i in range(len(N)):
+    
+        W += N[i]
 
 
 
