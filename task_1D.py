76d4616f8afe14ca8a9498718396e699802a89f3
Denchik444 <shtunderd07@mail.ru>
2024-12-03 15:51:17 +0000

    fh

--- task_1D.py
+++ task_1D.py
@@ -1,18 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-q = math.pi
-def ellipse(a=2, b=1):
-   x = np.arange(-5, 5, N)
-   y = np.arange(-5, 5, N)
+q = np.pi / 2
+def Lissashy(a=1, A=1, B=3, b=1, t=5):
+   x = np.arange(-5, 5, 0.01)
+   y = np.arange(-5, 5, 0.01)
 
    x, y = np.meshgrid(x, y)
    
-   fxy = (x**2)/(a**2) + (y**2)/(b**2)
+   fxy = A * np.sin(a * t * q)
    plt.contour(x, y, fxy, levels=[1])
    plt.axis('equal')
-   plt.savefig('fig_task_3.png')
+   plt.savefig('fig_task_1D.png')
 
 if __name__ == '__main__':
-    ellipse()
+    Lissashy()
