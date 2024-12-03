0686641db8dd0855afca4c682c4c37828509d7f0
Denchik444 <shtunderd07@mail.ru>
2024-12-03 19:54:45 +0000

    gh

--- task_4.py
+++ task_4.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import pi
+
+def polari(b=1, k=2):
+   fi = np.arange(0, 8*np.pi, 0.1)
+   r = k * fi
+   x = r * np.cos(fi)
+   y = r * np.sin(fi)
+
+   
+   
+   
+   plt.contour(x, y, levels=[1])
+   
+   plt.savefig('fig_task_4.png')
+
+if __name__ == '__main__':
+    polari()
 
